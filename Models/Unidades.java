6ede1bb5cdce9ad94c750711b7b7aeb502503a9a
Frank <navasfran98@gmail.com>
2021-06-02 01:46:16 -0600

    modelo unidad

--- Models/Unidades.java
+++ Models/Unidades.java
@@ -1,4 +1,57 @@
 package com.fiares.Models;
 
 public class Unidades {
+    private int id, orden_u, materia_id;
+    private String titulo, descripcion;
+
+    public Unidades(int id, int orden_u, int materia_id, String titulo, String descripcion) {
+        this.id = id;
+        this.orden_u = orden_u;
+        this.materia_id = materia_id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+
+    public Unidades() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrden_u() {
+        return orden_u;
+    }
+
+    public void setOrden_u(int orden_u) {
+        this.orden_u = orden_u;
+    }
+
+    public int getMateria_id() {
+        return materia_id;
+    }
+
+    public void setMateria_id(int materia_id) {
+        this.materia_id = materia_id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
